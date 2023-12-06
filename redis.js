de96bbb6e6049439cc0a38f9221bb98ff3e6959a
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-06 10:50:23 +0100

    upload fiiles

---
+++ redis.js
@@ -0,0 +1,38 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor () {
+        this.client = redis.createClient();
+        this.client.on('error', (err) => {
+            console.error(err);
+        })
+    }
+
+    isAlive() {
+        this.client.ping((err, resp) => {
+            if (resp === 'PONG') return true;
+            else return false;
+        })
+    }
+
+    async get(key) {
+        this.client.get(key, (err, resp) => {
+            if (err) console.error(err);
+            return resp;
+        })
+    }
+
+    async set(key, value, duration) {
+        this.client.setex(key, duration, value, (err, resp) => {
+            if (err) console.error(error);
+        })
+    }
+
+    async del(key) {
+        this.client.del(key, (err) => console.error(err))
+    }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
