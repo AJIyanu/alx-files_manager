82382776312cd077de496fd5fb4a7981cc673324
AJ Iyanu <aderemijoseph@live.com>
2023-12-08 19:31:15 +0100

    redis solved

--- utils/redis.js
+++ utils/redis.js
@@ -1,39 +1,35 @@
 const redis = require('redis');
+const utils = require('util');
 // import { createClient } from 'redis';
 
 class RedisClient {
-    constructor () {
-        this.client = redis.createClient();
-        this.connected = true;
-        this.client.on('error', (err) => {
-            this.connected = false;
-            console.error(err);
-        })
-        this.client.on("connect", () => {
-            this.connected = true;
-        })
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.connected = true;
+    this.client.on('error', (err) => {
+      this.connected = false;
+      console.error(err);
+    });
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+  }
 
-    isAlive() {
-        return this.connected;
-    }
+  isAlive() {
+    return this.connected;
+  }
 
-    async get(key) {
-        await this.client.get(key, (err, resp) => {
-            if (err) console.error(err);
-            return resp;
-        })
-    }
+  async get(key) {
+    return utils.promisify(this.client.GET).bind(this.client)(key);
+  }
 
-    async set(key, value, duration) {
-        await this.client.setex(key, duration, value, (err, resp) => {
-            if (err) console.error(error);
-        })
-    }
+  async set(key, value, duration) {
+    await this.client.setex(key, duration, value);
+  }
 
-    async del(key) {
-        await this.client.del(key, (err) => console.error(err))
-    }
+  async del(key) {
+    await this.client.del(key, (err) => console.error(err));
+  }
 }
 
 const redisClient = new RedisClient();
