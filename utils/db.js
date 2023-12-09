0e6a31a2becee3d8a67f3906263240a01660c2c6
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-09 03:50:14 +0100

    over to unbuntu

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+const mongoDB = require('mongodb');
+
+
+const dbHost = process.env.DB_HOST || "127.0.0.1";
+const dbPort = process.env.DB_PORT || "27017";
+const dbDatabase = process.env.DB_DATABASE || "files_manager";
+
+const uri = `mongodb://${dbHost}:${dbPort}/${dbDatabase}`;
+
+class DBClient {
+    constructor () {
+        this.client = new mongoDB.MongoClient(uri, { useUnifiedTopology: true });
+        this.client.connect();
+    }
+
+    isAlive () {
+        return this.client.isConnected();
+    }
+
+    async nbUsers () {
+        return this.client.db().collection('users').countDocuments();
+    }
+
+    async nbFiles () {
+        return this.client.db().collection('files').countDocuments();
+    }
+}
+
+
+const dbClient = new DBClient();
+module.exports = dbClient;
