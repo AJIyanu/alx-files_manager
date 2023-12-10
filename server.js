3097eaa5ed52f585807cce53d5e0cda5b5fdcaca
AJ Iyanu <aderemijoseph@live.com>
2023-12-10 04:36:09 +0100

    sync

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes/index');
 
 const app = express();
-cons port = process.env.PORT || '5000';
+const port = process.env.PORT || '5000';
 
 app.use('/', routes);
 
