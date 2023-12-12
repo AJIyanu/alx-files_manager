0db17090981286be0b76d7eac6658820d964d3ad
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-12 05:21:50 +0100

    check

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const dbClient = require('../utils/db');
+const crypto = require('crypto');
 
 
 class AuthController {
@@ -7,7 +8,20 @@ class AuthController {
         userCredentials = userCredentials.split(" ")[1]
         const bufObj = Buffer.from(userCredentials, "base64")
         userCredentials = bufObj.toString("utf-8");
-        res.status(200).json({check: userCredentials});
+        const email = userCredentials.split(":")[0];
+        const password = userCredentials.split(":")[1];
+        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex')
+
+        const user = dbClient.findUser( email );
+
+        if (!user) {
+            res.status(401).json({ error: Unauthorized });
+            return;
+        }
+
+        const verify = crypto.timingSafeEqual(Buffer.from(hashedPassword, 'hex'), Buffer.from(user.password, 'hex'));
+
+        res.status(200).json({ email: user.email, pwd: user.password, verify });
     }
 }
 
