a662642395df310e2eb794c75bc1c0d4e7a2fde0
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-12 08:40:17 +0100

    getconnect endpint

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
-const dbClient = require('../utils/db');
 const crypto = require('crypto');
 const uuidv4 = require('uuid').v4;
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis')
 
 class AuthController {
   static async getConnect(req, res) {
@@ -8,25 +9,25 @@ class AuthController {
     userCredentials = userCredentials.split(' ')[1];
     const bufObj = Buffer.from(userCredentials, 'base64');
     userCredentials = bufObj.toString('utf-8');
-    const email = userCredentials.split(':')[0];
-    const password = userCredentials.split(':')[1];
+    const [ email, password ] = userCredentials.split(':');
+    // const password = userCredentials.split(':')[1];
     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
 
     const user = await dbClient.findUser(email);
 
     if (!user) {
-      res.status(401).json({ error: Unauthorized });
+      res.status(401).json({ error: "Unauthorized" });
       return;
     }
 
     const verify = crypto.timingSafeEqual(Buffer.from(hashedPassword, 'hex'), Buffer.from(user.password, 'hex'));
     if (verify) {
-	   res.status(200).json({ token: uuidv4() });
-	   return;
+        const token = uuidv4()
+        await redisClient.set(`auth_${token}`, user._id, 24 * 60);
+        res.status(200).json({ token });
+    } else {
+        res.status(401).json({ error: "Unauthorized" });
     }
-    res.status(401).json({ error: Unauthorized });
-
-    res.status(200).json({ email: user.email, pwd: user.password, verify });
   }
 }
 
