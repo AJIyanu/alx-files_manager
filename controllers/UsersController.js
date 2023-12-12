31dc4391176997f733d0b8a1ae92e95061096f05
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-12 02:50:58 +0100

    insert email, hash password

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,4 @@
-const redisClient = require('../utils/redis');
+const crypto = require('crypto');
 const dbClient = require('../utils/db');
 
 class UsersController {
@@ -7,14 +7,25 @@ class UsersController {
         const email = req.body ? req.body.email : null;
         const password = req.body ? req.body.password : null;
 
-        console.log(email, password);
-
         if (!email) {
             res.status(400).json({error: "Missing email"});
+            return;
         }
         if (!password) {
             res.status(400).json({error: "Missing password"});
+            return;
         }
+
+        const user = await (await dbClient.usersCollection()).findOne({ email });
+        if ( user ) {
+            res.status(400).json({error: "Already exist"})
+            return;
+        }
+
+        const hashedPassword =crypto.createHash('sha1').update(password).digest('hex')
+
+        const newUser = await (await dbClient.usersCollection()).insertOne({ email: email, password: hashedPassword});
+        res.status(201).json({ email, id: newUser.insertedId.toString()});
     }
 }
 
