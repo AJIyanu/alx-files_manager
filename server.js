096e2e7a6248a04deb54335a96ce31014df0ded2
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-12 01:53:06 +0100

    added body parrser

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || '5000';
 
 app.use('/', routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
