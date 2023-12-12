9ef766a64cc7a9f3602c536a1f531fee7523be8b
AJ Iyanu <aderemijoseph@live.com>
2023-12-12 02:06:23 +0100

    ensured body parser

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const routes = require('./routes/index');
 const app = express();
 const port = process.env.PORT || '5000';
 
-app.use('/', routes);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
